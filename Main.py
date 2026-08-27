7398cbf08fa9fad1cf2cd259327abc82fefe8c70
JakeMidgley <jakesimpsonmidgley@gmail.com>
2026-08-26 17:29:45 -0700

    Basic trailing returns strategy w/ basic backtest

--- Main.py
+++ Main.py
@@ -4,8 +4,22 @@ import yfinance as yf
 import pandas as pd
 
 end_date = datetime(year=2025, month=12, day=31)
-start_date = datetime(year=2000, month=1, day=1)
+start_date = datetime(year=2015, month=1, day=1)
 
-tickers = ["NVDA", "MSFT", "AAPL"]
-data = yf.download(tickers, start=start_date, end=end_date, interval="1d", auto_adjust=True)
-print(data.head())
+#tickers = ["NVDA", "MSFT", "AAPL"]
+tickers = "AAPL"
+data = yf.download(tickers, start=start_date, end=end_date, interval="1d", auto_adjust=True,multi_level_index=False)
+
+#12-1 trailing return
+close21=data["Close"].shift(21)
+close252=data["Close"].shift(252)
+data["trailingReturn"] = ((close21 - close252) / close252 )*100
+
+#generate signals
+data["signal"] = 0
+data.loc[data['trailingReturn'] > 0, 'signal'] = 1  # Buy
+
+#backtest
+data["return"]= data["Close"].pct_change() * data["signal"].shift(1)
+data["cumulative_return"] = (1 + data["return"]).cumprod()
+print(data.tail())
