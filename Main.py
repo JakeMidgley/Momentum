df0bcb63e385e5a76248f70d6e3dad8968638fb4
JakeMidgley <jakesimpsonmidgley@gmail.com>
2026-08-27 11:40:44 -0700

    momentum strategy w/ backtest, sharpe, and max drawdown

--- Main.py
+++ Main.py
@@ -2,9 +2,10 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import yfinance as yf
 import pandas as pd
+import numpy as np
 
 end_date = datetime(year=2025, month=12, day=31)
-start_date = datetime(year=2015, month=1, day=1)
+start_date = datetime(year=2000, month=1, day=1)
 
 #tickers = ["NVDA", "MSFT", "AAPL"]
 tickers = "AAPL"
@@ -21,5 +22,30 @@ data.loc[data['trailingReturn'] > 0, 'signal'] = 1  # Buy
 
 #backtest
 data["return"]= data["Close"].pct_change() * data["signal"].shift(1)
-data["cumulative_return"] = (1 + data["return"]).cumprod()
-print(data.tail())
+data["cumulativeReturn"] = (1 + data["return"]).cumprod()
+#print(data.tail())
+
+#graph
+plt.figure(figsize=(12, 6))
+plt.plot(data['Close'], label='Stock Price', alpha=0.5)
+plt.plot(data.loc[data['signal'] == 1, 'Close'], '^', markersize=3, color='g', label='Buy Signal')
+plt.plot(data.loc[data['signal'] == 0, 'Close'], 'v', markersize=3, color='r', label='Sell Signal')
+
+plt.xlabel('Date')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
+
+#sharpe ratio
+meanReturn = data["return"].mean()
+stdReturn = data["return"].std(ddof=1)
+
+dailyRiskFree=0.05/252
+sharpeRatio = np.sqrt(len(data))*(meanReturn - dailyRiskFree) / stdReturn if stdReturn != 0 else 0
+print("Sharpe Ratio:", sharpeRatio)
+
+#max drawdown
+data["drawdown"]=(data["cumulativeReturn"]/data["cumulativeReturn"].cummax())-1
+mdd=data["drawdown"].min()*100
+print("Max Drawdown:", mdd, "%")
+
